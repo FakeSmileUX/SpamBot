a5628e16a799b19f82a95f9ab3cf5a0302ae8865
FakeSmile <47217704+FakeSmileUX@users.noreply.github.com>
2020-08-11 06:13:25 -0500

    Update check_modules.py

--- libs/check_modules.py
+++ libs/check_modules.py
@@ -27,13 +27,6 @@ def check_modules():
         sys.exit(0)
 
     try:
-        import proxybroker
-    except:
-        print("[-] 'proxybroker' package not installed!")
-        print("[*] For install 'pip install proxybroker' !")
-        sys.exit(0)
-
-    try:
         import warnings
     except:
         print("[-] 'warnings' package not installed!")
